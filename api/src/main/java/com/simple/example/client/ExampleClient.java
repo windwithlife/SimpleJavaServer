9d270e5189c96304f38880432d5baa608192dde6
windwithlife@hotmail.com <windwithlife@hotmail.com>
2020-10-19 22:09:19 +0800

    support client

--- api/src/main/java/com/simple/example/client/ExampleClient.java
+++ api/src/main/java/com/simple/example/client/ExampleClient.java
@@ -9,14 +9,12 @@ import com.simple.common.auth.AuthConstant;
 
 import javax.validation.Valid;
 
-@FeignClient(name = AccountConstant.SERVICE_NAME, path = "/v1/example", url = "${simple.example-service-endpoint}")
+@FeignClient(name = AccountConstant.SERVICE_NAME, path = "/simple/account", url = "${simple.example-service-endpoint}")
 // TODO Client side validation can be enabled as needed
 // @Validated
 public interface ExampleClient {
 
     @PostMapping(path = "/create")
     GenericAccountResponse createAccount(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String authz, @RequestBody @Valid CreateAccountRequest request);
-    // used after a user clicks a confirmation link in their email.
-    @GetMapping(path = "/test")
-    BaseResponse changeEmail(@RequestHeader(AuthConstant.AUTHORIZATION_HEADER) String authz, @RequestParam @Valid String request);
+
 }
